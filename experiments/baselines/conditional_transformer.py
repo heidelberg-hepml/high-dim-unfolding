corrected plots, separted the condition computation

experiments/baselines/conditional_transformer.py
@@ -223,20 +223,10 @@ class ConditionalTransformer(nn.Module):
     def forward(
         self,
         x: torch.Tensor,
-        condition: torch.Tensor,
+        processed_condition: torch.Tensor,
         attention_mask: Optional[torch.Tensor] = None,
-        attention_mask_condition: Optional[torch.Tensor] = None,
         crossattention_mask: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
-
-        condition = self.condition_linear_in(condition)
-        for block in self.condition_blocks:
-            if self.checkpoint_blocks:
-                condition = checkpoint(
-                    block, inputs=condition, attention_mask=attention_mask_condition
-                )
-            else:
-                condition = block(condition, attention_mask=attention_mask_condition)
 
         x = self.linear_in(x)
         for block in self.blocks:
@@ -244,16 +234,29 @@ class ConditionalTransformer(nn.Module):
                 x = checkpoint(
                     block,
                     inputs=x,
-                    condition=condition,
+                    condition=processed_condition,
                     attention_mask=attention_mask,
                     crossattention_mask=crossattention_mask,
                 )
             else:
                 x = block(
                     x,
-                    condition,
+                    processed_condition,
                     attention_mask=attention_mask,
                     crossattention_mask=crossattention_mask,
                 )
 
         return self.linear_out(x)
+
+    def forward_condition(
+        self, condition: torch.Tensor, attention_mask: Optional[torch.Tensor] = None
+    ) -> torch.Tensor:
+        condition = self.condition_linear_in(condition)
+        for block in self.condition_blocks:
+            if self.checkpoint_blocks:
+                condition = checkpoint(
+                    block, inputs=condition, attention_mask=attention_mask
+                )
+            else:
+                condition = block(condition, attention_mask=attention_mask)
+        return condition

experiments/unfolding/cfm.py
@@ -128,7 +128,8 @@ class CFM(nn.Module):
         xt_straight, vt_straight = self.geometry.get_trajectory(
             x0_straight, x1_straight, t
         )
-        vp_straight = self.get_velocity(xt_straight, t, batch)
+        condition = self.get_velocity_condition(batch)
+        vp_straight = self.get_velocity(xt_straight, t, batch, condition)
 
         # evaluate conditional flow matching objective
         distance = self.geometry.get_metric(
@@ -158,12 +159,14 @@ class CFM(nn.Module):
 
         sample_batch = batch.clone()
 
+        condition = self.get_velocity_condition(batch)
+
         def velocity(t, xt_straight):
             xt_straight = self.geometry._handle_periodic(xt_straight)
             t = t * torch.ones(
                 shape[0], 1, dtype=xt_straight.dtype, device=xt_straight.device
             )
-            vt_straight = self.get_velocity(xt_straight, t, batch)
+            vt_straight = self.get_velocity(xt_straight, t, batch, condition)
             vt_straight = self.handle_velocity(vt_straight)
             return vt_straight
 

experiments/unfolding/experiment.py
@@ -100,7 +100,7 @@ class UnfoldingExperiment(BaseExperiment):
         )
 
     def _init_data(self, Dataset, data_path):
-
+        t0 = time.time()
         data = energyflow.zjets_delphes.load(
             "Herwig",
             num_data=self.cfg.data.length,
@@ -108,7 +108,7 @@ class UnfoldingExperiment(BaseExperiment):
             cache_dir=data_path,
             include_keys=["particles", "mults", "jets"],
         )
-
+        LOGGER.info(f"Loaded data in {time.time() - t0:.2f} seconds")
         split = self.cfg.data.train_test_val
         size = len(data["sim_particles"])
         train_idx = int(split[0] * size)
@@ -357,6 +357,7 @@ class UnfoldingExperiment(BaseExperiment):
             )
             n_batches = len(loader)
         for i in range(n_batches):
+            t0 = time.time()
             batch = next(it).to(self.device)
             sample_batch = self.model.sample(
                 batch,
@@ -365,6 +366,7 @@ class UnfoldingExperiment(BaseExperiment):
             )
             samples.extend(sample_batch.to_data_list())
             targets.extend(batch.to_data_list())
+            LOGGER.info(f"Sampled batch {i+1} in {time.time() - t0:.2f}s")
 
         if self.cfg.data.embed_det_with_spurions:
             if len(self.spurions) > 0:
@@ -402,6 +404,7 @@ class UnfoldingExperiment(BaseExperiment):
         path = os.path.join(self.cfg.run_dir, f"plots_{self.cfg.run_idx}")
         os.makedirs(os.path.join(path), exist_ok=True)
         LOGGER.info(f"Creating plots in {path}")
+        t0 = time.time()
 
         if self.cfg.modelname == "ConditionalTransformer":
             model_label = "CondTr"
@@ -452,6 +455,7 @@ class UnfoldingExperiment(BaseExperiment):
                 plotter.plot_preprocessed(
                     filename=filename, **kwargs, weights=weights, mask_dict=mask_dict
                 )
+        LOGGER.info(f"Plotting done in {time.time() - t0:.2f} seconds")
 
     def _init_loss(self):
         # loss defined manually within the model
@@ -482,7 +486,7 @@ class UnfoldingExperiment(BaseExperiment):
 
         if "jet" in self.cfg.evaluation.observables:
 
-            def form_jet(constituents, batch_idx):
+            def form_jet(constituents, batch_idx, other_batch_idx=None):
                 jet = scatter(constituents, batch_idx, dim=0, reduce="sum")
                 return jet.cpu().detach()
 
@@ -502,11 +506,15 @@ class UnfoldingExperiment(BaseExperiment):
             for i in range(n_pt):
 
                 def select_pt(i):
-                    def ith_pt(constituents, index):
+                    def ith_pt(constituents, batch_idx, other_batch_idx):
                         idx = []
-                        batch_ptr = get_ptr_from_batch(index)
-                        for n in range(len(batch_ptr)):
-                            if i < batch_ptr[n + 1] - batch_ptr[n]:
+                        batch_ptr = get_ptr_from_batch(batch_idx)
+                        other_batch_ptr = get_ptr_from_batch(other_batch_idx)
+                        for n in range(len(batch_ptr) - 1):
+                            if (
+                                i < batch_ptr[n + 1] - batch_ptr[n]
+                                and i < other_batch_ptr[n + 1] - other_batch_ptr[n]
+                            ):
                                 idx.append(batch_ptr[n] + i)
                         selected_constituents = constituents[idx]
                         return selected_constituents.cpu().detach()
@@ -534,23 +542,3 @@ class UnfoldingExperiment(BaseExperiment):
                         [-5, -4],
                     ],
                 }
-
-                # def st_select_pt(i):
-                #     def ith_pt(constituents, index):
-                #         batch_ptr = get_ptr_from_batch(index)
-                #         idx = batch_ptr + i
-                #         selected_constituents = constituents[idx[:-1]]
-                #         # selected_constituents = (
-                #         #     selected_constituents
-                #         #     - selected_constituents.mean(dim=0, keepdim=True)
-                #         # ) / selected_constituents.std(dim=0, keepdim=True)
-                #         return selected_constituents.cpu().detach()
-
-                #     return ith_pt
-
-                # self.obs[str(i + 1) + " highest p_T"] = st_select_pt(i)
-                # self.obs_ranges[str(i + 1) + " highest p_T"] = {
-                #     "fourmomenta": [[-3, 3], [-3, 3], [-3, 3], [-3, 3]],
-                #     "jetmomenta": [[-3, 3], [-3, 3], [-3, 3], [-3, 3]],
-                #     "StandardLogPtPhiEtaLogM2": [[-3, 3], [-3, 3], [-3, 3], [-3, 3]],
-                # }

experiments/unfolding/wrappers.py
@@ -94,28 +94,30 @@ class ConditionalCFMForGA(EventCFM):
         assert (np.array(scalar_dims) < 4).all() and (np.array(scalar_dims) >= 0).all()
         self.cfg_data = cfg_data
 
-    def get_velocity(self, xt, t, batch):
+    def get_velocity_condition(self, batch):
+        attention_mask = xformers_sa_mask(batch.x_det_batch)
+        condition_mv, condition_s = self.net.forward_condition(
+            batch.x_det.unsqueeze(0), batch.scalars_det.unsqueeze(0), attention_mask
+        )
+        return condition_mv, condition_s
+
+    def get_velocity(self, xt, t, batch, condition):
         assert self.coordinates is not None
 
         fourmomenta = self.coordinates.x_to_fourmomenta(xt)
+        condition_mv, condition_s = condition
 
         mv, s = self.embed_into_ga(fourmomenta, batch.scalars_gen, t)
-        condition_mv, condition_s = batch.x_det, batch.scalars_det
 
-        # attention_mask = full_self_attention_mask(batch.x_gen_batch)
-        # attention_mask_condition = full_self_attention_mask(batch.x_det_batch)
-        # crossattention_mask = cross_attention_mask(batch.x_gen_batch, batch.x_det_batch)
         attention_mask = xformers_sa_mask(batch.x_gen_batch)
-        attention_mask_condition = xformers_sa_mask(batch.x_det_batch)
         crossattention_mask = xformers_sa_mask(batch.x_gen_batch, batch.x_det_batch)
 
         mv_outputs, s_outputs = self.net(
             multivectors=mv.unsqueeze(0),
-            multivectors_condition=condition_mv.unsqueeze(0),
+            multivectors_condition=condition_mv,
             scalars=s.unsqueeze(0),
-            scalars_condition=condition_s.unsqueeze(0),
+            scalars_condition=condition_s,
             attention_mask=attention_mask,
-            attention_mask_condition=attention_mask_condition,
             crossattention_mask=crossattention_mask,
         )
         mv_outputs = mv_outputs.squeeze(0)
@@ -152,24 +154,28 @@ class ConditionalTransformerCFM(EventCFM):
         )
         self.net = net
 
-    def get_velocity(self, xt, t, batch):
+    def get_velocity_condition(self, batch):
+        condition_x = self.condition_coordinates.fourmomenta_to_x(batch.x_det)
+        condition = torch.cat([condition_x, batch.scalars_det], dim=-1)
+        attention_mask = xformers_sa_mask(batch.x_det_batch)
+        processed_condition = self.net.forward_condition(
+            condition=condition.unsqueeze(0),
+            attention_mask=attention_mask,
+        )
+        return processed_condition
 
+    def get_velocity(self, xt, t, batch, processed_condition):
         t_embedding = self.t_embedding(t)
 
         x = torch.cat([xt, batch.scalars_gen, t_embedding], dim=-1)
 
-        condition_x = self.condition_coordinates.fourmomenta_to_x(batch.x_det)
-        condition = torch.cat([condition_x, batch.scalars_det], dim=-1)
-
         attention_mask = xformers_sa_mask(batch.x_gen_batch)
-        attention_mask_condition = xformers_sa_mask(batch.x_det_batch)
         crossattention_mask = xformers_sa_mask(batch.x_gen_batch, batch.x_det_batch)
 
         v = self.net(
             x=x.unsqueeze(0),
-            condition=condition.unsqueeze(0),
+            processed_condition=processed_condition,
             attention_mask=attention_mask,
-            attention_mask_condition=attention_mask_condition,
             crossattention_mask=crossattention_mask,
         )
         v = v.squeeze(0)

gatr/nets/conditional_gatr.py
@@ -143,6 +143,35 @@ class ConditionalGATr(nn.Module):
         )
         self._checkpoint_blocks = checkpoint_blocks
 
+    def forward_condition(
+        self,
+        multivectors_condition: torch.Tensor,
+        scalars_condition: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+    ) -> Tuple[torch.Tensor, Union[torch.Tensor, None]]:
+
+        # Encode condition with GATr blocks
+        c_mv, c_s = self.linear_in_condition(
+            multivectors_condition, scalars=scalars_condition
+        )
+        for block in self.condition_blocks:
+            if self._checkpoint_blocks:
+                c_mv, c_s = checkpoint(
+                    block,
+                    c_mv,
+                    use_reentrant=False,
+                    scalars=c_s,
+                    attention_mask=attention_mask,
+                )
+            else:
+                c_mv, c_s = block(
+                    c_mv,
+                    scalars=c_s,
+                    attention_mask=attention_mask,
+                )
+
+        return c_mv, c_s
+
     def forward(
         self,
         multivectors: torch.Tensor,
@@ -150,7 +179,6 @@ class ConditionalGATr(nn.Module):
         scalars: Optional[torch.Tensor] = None,
         scalars_condition: Optional[torch.Tensor] = None,
         attention_mask: Optional[torch.Tensor] = None,
-        attention_mask_condition: Optional[torch.Tensor] = None,
         crossattention_mask: Optional[torch.Tensor] = None,
     ) -> Tuple[torch.Tensor, Union[torch.Tensor, None]]:
         """Forward pass of the network.
@@ -180,26 +208,6 @@ class ConditionalGATr(nn.Module):
             Output scalars, if scalars are provided. Otherwise None.
         """
 
-        # Encode condition with GATr blocks
-        c_mv, c_s = self.linear_in_condition(
-            multivectors_condition, scalars=scalars_condition
-        )
-        for block in self.condition_blocks:
-            if self._checkpoint_blocks:
-                c_mv, c_s = checkpoint(
-                    block,
-                    c_mv,
-                    use_reentrant=False,
-                    scalars=c_s,
-                    attention_mask=attention_mask_condition,
-                )
-            else:
-                c_mv, c_s = block(
-                    c_mv,
-                    scalars=c_s,
-                    attention_mask=attention_mask_condition,
-                )
-
         # Decode condition into main track with
         h_mv, h_s = self.linear_in(multivectors, scalars=scalars)
         for block in self.blocks:
@@ -209,8 +217,8 @@ class ConditionalGATr(nn.Module):
                     h_mv,
                     use_reentrant=False,
                     scalars=h_s,
-                    multivectors_condition=c_mv,
-                    scalars_condition=c_s,
+                    multivectors_condition=multivectors_condition,
+                    scalars_condition=scalars_condition,
                     attention_mask=attention_mask,
                     crossattention_mask=crossattention_mask,
                 )
@@ -218,8 +226,8 @@ class ConditionalGATr(nn.Module):
                 h_mv, h_s = block(
                     h_mv,
                     scalars=h_s,
-                    multivectors_condition=c_mv,
-                    scalars_condition=c_s,
+                    multivectors_condition=multivectors_condition,
+                    scalars_condition=scalars_condition,
                     attention_mask=attention_mask,
                     crossattention_mask=crossattention_mask,
                 )