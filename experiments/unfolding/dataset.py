8e330ddcba7f16aea0d1cb311c2b5a301d1d025b
AntoinePTJ <antoine.petitjean1@ens-lyon.fr>
2025-04-14 14:51:42 +0200

    added dataset loading functions

--- experiments/unfolding/dataset.py
+++ experiments/unfolding/dataset.py
@@ -1,13 +1,21 @@
 import torch
 from torch_geometric.data import Data
+import energyflow
+import numpy as np
+import os
 
 from gatr.interface import embed_vector
-from experiments.unfolding.utils import get_pt
+from experiments.unfolding.utils import (
+    get_pt,
+    ensure_angle,
+    pid_encoding,
+    jetmomenta_to_fourmomenta,
+)
 from experiments.unfolding.embedding import event_to_GA_with_spurions
 from experiments.logger import LOGGER
 
 
-class ZplusJetDataset(torch.utils.data.Dataset):
+class Dataset(torch.utils.data.Dataset):
     def __init__(self, dtype, embed_into_GA=False, spurions=None):
         self.dtype = dtype
         self.embed_into_GA = embed_into_GA
@@ -28,22 +36,20 @@ class ZplusJetDataset(torch.utils.data.Dataset):
         gen_particles,
         gen_pids,
         gen_mults,
+        fourm=True,
     ):
 
         self.data_list = []
         for i in range(det_particles.shape[0]):
-            # det_pt = get_pt(det_particles[i])
-            # gen_pt = get_pt(gen_particles[i])
+            if fourm:
+                det_pt = get_pt(det_particles[i])
+                gen_pt = get_pt(gen_particles[i])
+            else:
+                det_pt = det_particles[i, :, 0]
+                gen_pt = gen_particles[i, :, 0]
 
-            # det_idx = torch.argsort(det_pt, descending=True)[: det_mults[i]]
-            # gen_idx = torch.argsort(gen_pt, descending=True)[: gen_mults[i]]
-
-            det_idx = torch.argsort(det_particles[i, :, 0], descending=True)[
-                : det_mults[i]
-            ]
-            gen_idx = torch.argsort(gen_particles[i, :, 0], descending=True)[
-                : gen_mults[i]
-            ]
+            det_idx = torch.argsort(det_pt, descending=True)[: det_mults[i]]
+            gen_idx = torch.argsort(gen_pt, descending=True)[: gen_mults[i]]
 
             det_event = det_particles[i, det_idx]
             det_scalars = det_pids[i, det_idx]
@@ -66,3 +72,85 @@ class ZplusJetDataset(torch.utils.data.Dataset):
             )
 
             self.data_list.append(graph)
+
+
+def load_zplusjet(data_path, cfg, dtype):
+    data = energyflow.zjets_delphes.load(
+        "Herwig",
+        num_data=cfg.data.num_data,
+        pad=True,
+        cache_dir=data_path,
+        include_keys=["particles", "mults", "jets"],
+    )
+    size = len(data["sim_particles"])
+
+    det_particles = torch.tensor(data["sim_particles"], dtype=dtype)
+    det_jets = torch.tensor(data["sim_jets"], dtype=dtype)
+    det_mults = torch.tensor(data["sim_mults"], dtype=torch.int)
+
+    gen_particles = torch.tensor(data["gen_particles"], dtype=dtype)
+    gen_jets = torch.tensor(data["gen_jets"], dtype=dtype)
+    gen_mults = torch.tensor(data["gen_mults"], dtype=torch.int)
+
+    # undo the dataset scaling
+    det_particles[..., 1:3] = det_particles[..., 1:3] + det_jets[:, None, 1:3]
+    det_particles[..., 2] = ensure_angle(det_particles[..., 2])
+    det_particles[..., 0] = det_particles[..., 0] * 100
+
+    gen_particles[..., 1:3] = gen_particles[..., 1:3] + gen_jets[:, None, 1:3]
+    gen_particles[..., 2] = ensure_angle(gen_particles[..., 2])
+    gen_particles[..., 0] = gen_particles[..., 0] * 100
+
+    # swap eta and phi for consistency
+    det_particles[..., [1, 2]] = det_particles[..., [2, 1]]
+    gen_particles[..., [1, 2]] = gen_particles[..., [2, 1]]
+
+    # save pids before replacing with mass
+    if cfg.data.pid_encoding:
+        det_pids = det_particles[..., 3].clone().unsqueeze(-1)
+        det_pids = pid_encoding(det_pids)
+        gen_pids = gen_particles[..., 3].clone().unsqueeze(-1)
+        gen_pids = pid_encoding(gen_pids)
+    else:
+        det_pids = torch.empty(*det_particles.shape[:-1], 0, dtype=dtype)
+        gen_pids = torch.empty(*gen_particles.shape[:-1], 0, dtype=dtype)
+
+    det_particles[..., 3] = cfg.data.mass**2
+    gen_particles[..., 3] = cfg.data.mass**2
+
+    det_particles = jetmomenta_to_fourmomenta(det_particles)
+    gen_particles = jetmomenta_to_fourmomenta(gen_particles)
+
+    return {
+        "det_particles": det_particles,
+        "det_mults": det_mults,
+        "det_pids": det_pids,
+        "gen_particles": gen_particles,
+        "gen_mults": gen_mults,
+        "gen_pids": gen_pids,
+    }
+
+
+def load_cms(data_path, cfg, dtype):
+    gen_particles = (
+        torch.from_numpy(np.load(os.path.join(data_path, "gen_1725_delphes.npy")))
+        .to(dtype)
+        .reshape(-1, 3, 4)
+    )
+    det_particles = (
+        torch.from_numpy(np.load(os.path.join(data_path, "rec_1725_delphes.npy")))
+        .to(dtype)
+        .reshape(-1, 3, 4)
+    )
+    gen_mults = torch.ones(gen_particles.shape[0], dtype=torch.int)
+    det_mults = torch.ones(det_particles.shape[0], dtype=torch.int)
+    gen_pids = torch.empty(*gen_particles.shape[:-1], 0, dtype=dtype)
+    det_pids = torch.empty(*det_particles.shape[:-1], 0, dtype=dtype)
+    return {
+        "det_particles": det_particles,
+        "det_mults": det_mults,
+        "det_pids": det_pids,
+        "gen_particles": gen_particles,
+        "gen_mults": gen_mults,
+        "gen_pids": gen_pids,
+    }
