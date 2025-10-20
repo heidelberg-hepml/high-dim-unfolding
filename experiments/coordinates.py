Fix scaling and Von Mises

experiments/coordinates.py
@@ -3,6 +3,7 @@ from torch.distributions import VonMises
 
 import experiments.transforms as tr
 from experiments.utils import kappa_from_Vc
+from experiments.logger import LOGGER
 
 DTYPE = torch.float64
 
@@ -19,6 +20,7 @@ class BaseCoordinates(torch.nn.Module):
         self.contains_phi = False
         self.contains_mass = False
         self.transforms = []
+        self.vonmises = False
 
     def init_fit(self, fourmomenta, mask=None, **kwargs):
         if mask is None:
@@ -34,7 +36,7 @@ class BaseCoordinates(torch.nn.Module):
             ).all(), (
                 f"Transform {transform.__class__.__name__} produced non-finite values."
             )
-        if self.contains_phi:
+        if self.vonmises:
             c = torch.cos(x[mask][:, 1]).mean()
             s = torch.sin(x[mask][:, 1]).mean()
             loc = torch.atan2(s, c).item()
@@ -42,6 +44,10 @@ class BaseCoordinates(torch.nn.Module):
             concentration = kappa_from_Vc(circular_var.item())
             self.phi_dist = VonMises(loc=loc, concentration=concentration)
             self.phi_std = x[mask][:, 1].std().item()
+            try:
+                setattr(self.transforms[-1], "contains_phi", False)
+            except:
+                pass
         self.transforms[-1].init_fit(x, mask=mask, **kwargs)
 
     def fourmomenta_to_x(self, a_in, **kwargs):

experiments/kinematics/cfm.py
@@ -80,7 +80,10 @@ class CFM(nn.Module):
         )
         if self.const_coordinates.contains_phi:
             if getattr(self.cfm.const_coordinates_options, "vonmises", False):
-                sample[..., 1] = self.const_coordinates.phi_dist.sample(x0.shape[:-1])
+                sample[..., 1] = (
+                    self.const_coordinates.phi_dist.sample(x0.shape[:-1])
+                    / self.const_coordinates.phi_std
+                )
             elif "JetScaled" not in self.cfm.const_coordinates:
                 sample[..., 1] = (
                     torch.rand(
@@ -133,7 +136,9 @@ class CFM(nn.Module):
         loss : torch.tensor with shape (1)
         """
         if self.cfm.add_jet:
-            new_batch, constituents_mask, _ = add_jet_to_sequence(batch)
+            new_batch, constituents_mask, det_constituents_mask = add_jet_to_sequence(
+                batch
+            )
         else:
             new_batch = batch
             constituents_mask = torch.ones(
@@ -380,14 +385,18 @@ class EventCFM(CFM):
         self.condition_const_coordinates = getattr(c, self.cfm.const_coordinates)(
             **self.cfm.const_coordinates_options
         )
-        if getattr(self.cfm.const_coordinates_options, "vonmises", False):
-            self.scaling[1] = self.scaling[1] / self.const_coordinates.phi_std
         self.jet_coordinates = getattr(c, self.cfm.jet_coordinates)(
             **self.cfm.jet_coordinates_options
         )
         self.condition_jet_coordinates = getattr(c, self.cfm.jet_coordinates)(
             **self.cfm.jet_coordinates_options
         )
+        if self.cfm.const_coordinates_options.vonmises:
+            self.condition_const_coordinates.vonmises = True
+            self.const_coordinates.vonmises = True
+        if self.cfm.jet_coordinates_options.vonmises:
+            self.jet_coordinates.vonmises = True
+            self.condition_jet_coordinates.vonmises = True
         if self.cfm.transforms_float64:
             self.const_coordinates.to(torch.float64)
             self.condition_const_coordinates.to(torch.float64)
@@ -395,12 +404,17 @@ class EventCFM(CFM):
             self.condition_jet_coordinates.to(torch.float64)
 
     def init_geometry(self):
+
         # placeholder for any initialization that needs to be done
+        if self.cfm.const_coordinates_options.vonmises:
+            scale = self.scaling[1].item() / self.const_coordinates.phi_std
+        else:
+            scale = self.scaling[1].item()
         if self.cfm.geometry.type == "simple":
             self.geometry = SimplePossiblyPeriodicGeometry(
                 contains_phi=self.const_coordinates.contains_phi,
                 periodic=self.cfm.geometry.periodic,
-                scale=self.scaling[1].item(),
+                scale=scale,
             )
         else:
             raise ValueError(f"geometry={self.cfm.geometry} not implemented")

experiments/kinematics/experiment.py
@@ -222,9 +222,6 @@ class KinematicsExperiment(BaseExperiment):
         # initialize cfm (might require data)
         self.model.init_coordinates()
 
-        # initialize geometry
-        self.model.init_geometry()
-
         train_gen_mask = (
             torch.arange(gen_particles.shape[1])[None, :] < gen_mults[:train_idx, None]
         )
@@ -250,6 +247,9 @@ class KinematicsExperiment(BaseExperiment):
         )
 
         self.model.condition_jet_coordinates.init_fit(det_jets[:train_idx])
+
+        # initialize geometry
+        self.model.init_geometry()
 
         gen_mask = torch.arange(gen_particles.shape[1])[None, :] < gen_mults[:, None]
         gen_particles[gen_mask] = self.model.const_coordinates.fourmomenta_to_x(